f3ef2314b64c4a9a490c05ab4cb6c9b7a698414d
Piyumi <104864863+PiyumiNavo@users.noreply.github.com>
2024-07-23 10:22:21 +0530

    Update q3.scala

--- q3.scala
+++ q3.scala
@@ -1,8 +1,8 @@
 @main def main() = {
     println(formatNames("Benny", toUpper))        
-    println(formatNames("Niroshan", customFormat)) 
+    println(formatNames("Niroshan", customFormat1)) 
     println(formatNames("Saman", toLower))         
-    println(formatNames("Kumara", anotherFormat))  
+    println(formatNames("Kumara", customFormat2))  
 }
 
 
@@ -13,13 +13,13 @@ def toLower(word: String): String = word.toLowerCase
 def formatNames(name: String, formatFunc: String => String): String = formatFunc(name)
 
 
-def customFormat(name: String): String = {
+def customFormat1(name: String): String = {
     val chars = name.toCharArray
     if (chars.length > 1) chars(1) = chars(1).toUpper
     new String(chars)
 }
 
-def anotherFormat(name: String): String = {
+def customFormat2(name: String): String = {
     val chars = name.toCharArray
     if (chars.length > 5) chars(5) = chars(5).toUpper
     new String(chars)
